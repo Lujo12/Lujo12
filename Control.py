7767c90e99e1194abed2553f2777710950e3af98
Lujo12 <60344378+Lujo12@users.noreply.github.com>
2020-12-29 12:32:24 +0200

    Control.py
    A code to take in a user’s age using input() and store their age in an
    integer variable called age.
    ● Then check if the user’s age is over 18. If the user is over 18, print out the
    message “You are old enough!” else if they are over 16 print “Almost there”,
    otherwise print “You’re just too young!” You should use one if, elif and else
    statement to do this.

---
+++ Control.py
@@ -0,0 +1,8 @@
+age = int(input("request user to enter age:"))
+if (age > 18):
+    print (f"You are old enough")
+elif (age > 16):
+    print (f"Almost there")
+else:
+    print (f"You're just too young")
+
