986a5e890e0a2badb8eefe8eb6f55243e780a5bf
Lujo12 <60344378+Lujo12@users.noreply.github.com>
2020-12-29 12:50:55 +0200

    logic.py
    A program that displays a logical error (be as creative as possible!).

---
+++ logic.py
@@ -0,0 +1,8 @@
+x = float(input('Enter a number: '))
+y = float(input('Enter a number: '))
+z = x+y/2
+# the order of operations in arithmetic (the division is evaluated before addition) the program will not give the right answer:
+print ('The average of the two numbers you have entered is:',z) 
+
+# To rectify this problem, we will simply add the parentheses: z = (x+y)/2
+z = (x+y)/2
