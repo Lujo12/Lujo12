c2fd600d76169a3557eed1349b2f9e55e3a0ca8f
Lujo12 <60344378+Lujo12@users.noreply.github.com>
2020-12-29 13:05:36 +0200

    Optional_task.py
    a text file called ‘numbers1.txt’ that contains Integers which are
    sorted from smallest to largest.
    ● Create another text file called ‘numbers2.txt’ which also contains Integers
    that are sorted from smallest to largest.
    ● Write the numbers from both files to a third file called ‘allNumbers.txt’
    ● All the numbers in ‘allNumbers.txt’ should be sorted from smallest to
    largest.

--- Optional_task.py
+++ Optional_task.py
@@ -1,21 +1,6 @@
-import string
-chars = []
-with open("input.txt", "r") as f:
-    for c in f.read():
-        chars.append(c)
-num_chars = len(chars)
-num_words = 0
-num_vowels = 0
-num_lines = 0
-vowels = "aeiouAEIOU"
-for c in chars:
-    if c in vowels:
-        num_vowels += 1
-    if c in string.num_words:
-        num_words += 1
-    if c in string.num_lines:
-        num_lines += 1
-print(num_chars)
-print(num_lines)
-print(num_words)
-print(num_vowels)
+ofile = open('numbers1.txt' 'numbers2.txt', 'w+') #openning the two text file with integers
+numbers = {"numbers1.txt" "numbers2.txt"}
+sorted(numbers) #sorting the numbers from the largset to trhe smallest
+numbers
+ofile.write ("The numbers are on the line above in this text file") # write   idNum to ofile 
+ofile.close() #closing the file 
